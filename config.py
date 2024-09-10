928c93c16f8d7312709dcd5a1008d3f8e00bc27b
Satpal1414 <satpla123321@gmail.com>
2024-09-10 11:57:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7108927415:AAE-asOaj8FaRufvr7tROm3qSYZS0e7en7g")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("hackerofblax")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1087563043")
 
 
 
